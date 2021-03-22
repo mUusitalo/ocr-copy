84bfb8a0e388faae2b19069eb910c1dc959583d3
mUusitalo <73437699+mUusitalo@users.noreply.github.com>
2021-03-22 16:06:04 +0200

    Refactor settings

--- settings.py
+++ settings.py
@@ -1,3 +1,4 @@
+from copy import copy
 import functools
 import json
 import pathlib
@@ -8,16 +9,13 @@ from pynput import keyboard as kb
 
 print = functools.partial(print, flush=True)
 
-SETTINGS_PATH = 'settings.json'
+class InputError(Exception):
+    pass
 
-# Default settings
-TESSERACT_PATH = str(pathlib.Path('C:/Program Files/Tesseract-OCR/tesseract.exe'))
-EXIT_HOTKEY = '<shift>+<esc>'
-CAPTURE_HOTKEY = '<alt>+s'
-LANGUAGES = 'eng'
+SETTINGS_PATH: str = 'settings.json'
 
 # Available languages are listed here because pytesseract.get_languages() requires a valid path to tesseract.exe
-AVAILABLE_LANGUAGES = {
+AVAILABLE_LANGUAGES: set[str] = {
     'afr', 'amh', 'ara', 'asm', 'aze', 'aze_cyrl', 'bel', 'ben', 'bod',
     'bos', 'bre', 'bul', 'cat', 'ceb', 'ces', 'chi_sim', 'chi_sim_vert',
     'chi_tra', 'chi_tra_vert', 'chr', 'cos', 'cym', 'dan', 'deu', 'div',
@@ -33,142 +31,160 @@ AVAILABLE_LANGUAGES = {
     'tha', 'tir', 'ton', 'tur', 'uig', 'ukr', 'urd', 'uzb', 'uzb_cyrl',
     'vie', 'yid', 'yor'}
 
+def get_available_languages() -> set[str]:
+    try:
+        # Will throw an exception if tesseract_cmd is invalid
+        return set(tess.get_languages())
+    except EnvironmentError:
+        return AVAILABLE_LANGUAGES
+
+def test_hotkey(hotkey: str = ''):
+    if not hotkey: return hotkey
+    try:
+        kb.HotKey.parse(hotkey)
+        return hotkey
+    except:
+        raise InputError(f"Invalid key: {hotkey}")
+
 class Settings:
-    """
-    Singleton class for storing, reading, and writing settings. All settings have getters and setters that make sure the settings are valid.
-    Will throw errors if settings are invalid.
-    """
 
-    _instance = None
+    all_settings = ['capture_hotkey', 'exit_hotkey', 'tesseract_path', 'languages']
 
-    def __new__(cls):
-        cls._instance = cls._instance or super().__new__(cls)._init()
-        return cls._instance
-            
-    def _init(self):
-        try:
-            self._read()
-        except Exception as e:
-            print(e)
-            self._reset()
-        return self
 
-    def __dir__(self):
-        return ['capture_hotkey', 'exit_hotkey', 'tesseract_path', 'languages']
+    def __init__(self, 
+                 capture_hotkey: str='', exit_hotkey: str='',
+                 tesseract_path: str='', languages: str='', verbose=True):
+        self._verbose: bool = verbose
+        self.capture_hotkey: str = capture_hotkey
+        self.exit_hotkey: str = exit_hotkey
+        self.tesseract_path: str = tesseract_path
+        self.languages: str = languages
 
     def __iter__(self):
-        for attr in dir(self):
-            yield attr, getattr(self, attr, '')
+        for setting in self.all_settings:
+            yield setting, getattr(self, setting)
 
     def __str__(self):
         return '\n'.join(f"    {setting}: {value or '-'}" for setting, value in self)
 
     @staticmethod
-    def test_hotkey(hotkey: str) -> None:
-        kb.HotKey.parse(hotkey)
-        return hotkey
-
-    def _prompt(self, attribute: str) -> str:
-        print(f"\nCurrent {attribute}: {getattr(self, attribute, None)}")
-        output = input(f"Enter {attribute}: ")
-        if not output: return # Setting skipped
-        try:
-            setattr(self, attribute, output.strip())
-        except Exception as e:
-            print(e)
-            self._prompt(attribute)
-
-    def _reset(self):
-        try: self.tesseract_path = TESSERACT_PATH
-        except: self.tesseract_path = None
-
-        try: self.languages = LANGUAGES
-        except: self.languages = None
-        
-        try: self.exit_hotkey = EXIT_HOTKEY
-        except: self.exit_hotkey = None
-        
-        try: self.capture_hotkey = CAPTURE_HOTKEY
-        except: self.capture_hotkey = None
-
-    def _dialog(self):
-        print(f"\nCurrent settings:\n{str(self)}\n")
-        reset = input("Restore defaults? (y/N):")
-        if reset == 'y':
-            self._reset()
-            print(f"\nCurrent settings:\n{str(self)}\n")
-        print("Enter settings. Leave blank to skip and enter one space to clear.")
-
-        for attribute in (attribute for attribute in dir(self) if not attribute.startswith('_')):
-            self._prompt(attribute)
-        print(f"\nCurrent settings:\n{str(self)}\n")
-        return self
-
-    def _read(self) -> 'Settings':
-        with open(SETTINGS_PATH, 'r') as settings_file:
-            settings_dict = json.loads(settings_file.read())
-        sorted_settings_dict = dict(sorted(settings_dict.items(), key=lambda item: dir(self).index(item[0]))) # This is broken: dir() apparently sorts alphabetically.
-        for key, value in sorted_settings_dict.items():
-            if key in dir(self):
+    def from_dict(settings_dict) -> 'Settings':
+        settings = Settings()
+        for key, value in settings_dict.items():
+            if key in Settings.all_settings:
                 try:
-                    setattr(self, key, value)
-                except:
-                    setattr(self, key, None)
+                    setattr(settings, key, value)
+                except InputError:
+                    pass
+        return settings
 
-    def _write(self):
-        settings_dict = {attr: getattr(self, attr, None) for attr in dir(self)}
+    @staticmethod
+    def from_file() -> 'Settings':
+        try:
+            with open(SETTINGS_PATH, 'r') as settings_file:
+                settings_dict = json.loads(settings_file.read())
+            return Settings.from_dict(settings_dict)
+        except:
+            print("Settings file is missing or corrupted.")
+            return copy(DEFAULT_SETTINGS)
+
+    @staticmethod
+    def to_file(settings: 'Settings'):
+        settings_dict = {setting: value for setting, value in settings}
         with open(SETTINGS_PATH, 'w') as settings_file:
             settings_file.write(json.dumps(settings_dict))
 
+    def _prompt_setting(self, setting: str):
+        print(f"\nCurrent {setting}: {getattr(self, setting) or '-'}")
+        output = input(f"Enter {setting}: ")
+        if not output: return # Setting skipped
+        try:
+            setattr(self, setting, output.strip())
+        except InputError as e:
+            print(e)
+            self._prompt_setting(setting)
+
+    def _prompt_reset(self) -> None:
+        reset = input("Restore defaults? (y/N):")
+        if reset == 'y':
+            for key, value in DEFAULT_SETTINGS:
+                try:
+                    setattr(self, key, value)
+                except InputError:
+                    pass
+            print(f"\nCurrent settings:\n{str(self)}\n")
+            
+    def run_dialog(self) -> None:
+        print(f"\nCurrent settings:\n{str(self)}\n")
+        self._prompt_reset()
+        print("Enter settings. Leave blank to skip and enter one space to clear.")
+        for setting, _ in self:
+            self._prompt_setting(setting)
+        print(f"\nCurrent settings:\n{str(self)}\n")
+
     @property
     def tesseract_path(self) -> str:
-        return getattr(self, '_tesseract_path', '')
+        return self._tesseract_path
+
+    def _warn_no_connection(self) -> None:
+        if not self._verbose: return
+        print("The current path to tesseract.exe is invalid. Running the program now will produce a runtime error.")
 
     @tesseract_path.setter
     def tesseract_path(self, input_path: str) -> None:
         if not input_path:
             self._tesseract_path = ''
+            self._warn_no_connection()
         else:
             path = str(pathlib.Path(input_path))
             tess.pytesseract.tesseract_cmd = path
-            print(f"Tesseract version: {tess.get_tesseract_version()}") # Will throw error if path is invalid
-            self._tesseract_path = path
+            try:
+                tesseract_version = tess.get_tesseract_version() # Will throw error if path is invalid
+            except EnvironmentError as e:
+                raise InputError(str(e))
+            if self._verbose:
+                print(f"Tesseract version: {tesseract_version}")
+            self._tesseract_path: str = path
     
     @property
     def languages(self) -> str:
-        return getattr(self, '_languages', '')
+        return self._languages
 
     @languages.setter
     def languages(self, input_languages: str) -> None:
-        input_languages = input_languages or '' # Change None to str
-        lang_list = re.findall(r'(?i)[\w_]+', input_languages)
-        try:
-            # Will throw an exception if self._tesseract_path is invalid
-            available_languages = set(tess.get_languages())
-        except:
-            available_languages = AVAILABLE_LANGUAGES
+        lang_list: list[str] = re.findall(r'(?i)[\w_]+', input_languages)
+        available_languages: set[str] = get_available_languages()
         for lang in lang_list:
             if not lang.lower() in available_languages:
-                raise ValueError(f"Unexpected input: {lang}")
-        self._languages = '+'.join(lang_list).lower()
+                raise InputError(f"Unexpected input: {lang}")
+        self._languages: str = '+'.join(lang_list).lower()
 
     @property
     def exit_hotkey(self) -> str:
-        return getattr(self, '_exit_hotkey', '')
+        return self._exit_hotkey
     
     @exit_hotkey.setter
     def exit_hotkey(self, hotkey: str) -> None:
-        self._exit_hotkey = self.test_hotkey(hotkey) if hotkey else ''
+        self._exit_hotkey: str = test_hotkey(hotkey)
 
     @property
     def capture_hotkey(self) -> str:
-        return getattr(self, '_capture_hotkey', '')
+        return self._capture_hotkey
 
     @capture_hotkey.setter
     def capture_hotkey(self, hotkey: str) -> None:
-        self._capture_hotkey = self.test_hotkey(hotkey) if hotkey else ''
+        self._capture_hotkey: str = test_hotkey(hotkey)
 
-Settings() # Create singleton to intialize it
+DEFAULT_SETTINGS: Settings = Settings(
+    tesseract_path=str(pathlib.Path('C:/Program Files/Tesseract-OCR/tesseract.exe')),
+    exit_hotkey='<shift>+<esc>',
+    capture_hotkey='<alt>+s',
+    languages='eng',
+    verbose=False
+)
+
+settings = Settings().from_file()
 
 if __name__ == '__main__':
-    Settings()._dialog()._write()
+    settings.run_dialog()
+    Settings.to_file(settings)
--- src/analyze.py
+++ src/analyze.py
@@ -1,10 +1,10 @@
 import pytesseract as tess
 from pyzbar import pyzbar
-from settings import Settings
+from settings import settings
 
 def analyze(image):
     data = pyzbar.decode(image)
     if data:
         return data[0].data.decode()
     else:
-        return tess.image_to_string(image, lang=Settings().languages).strip()
+        return tess.image_to_string(image, lang=settings.languages).strip()
